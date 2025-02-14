a7e92f8b600ff0d2e08e3c6dde163869972d03bf
meghanak <karimeghana02@gmail.com>
2025-02-14 11:48:16 +0530

    Completed Polymorphism Code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+    public static class Product {
+        public int Product(int x, int y) {
+            return x * y;
+        }
 
+        public int Product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double Product(double x, double y) {
+            return x * y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        System.out.println(p.Product(3,4));
+        System.out.println(p.Product(3, 4, 5));
+        System.out.println(p.Product(3.5, 6.3));
+    }
 }
